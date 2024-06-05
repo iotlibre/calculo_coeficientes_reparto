e78b243abcf320f5d22deb6493359e9cb641d8b8
Jose Naveiro <jmnavegomez@gmail.com>
2024-06-05 14:11:38 +0200

    Cambios en el ignore y en la ruta del bash

--- servicio_comunidades_v2.sh
+++ servicio_comunidades_v2.sh
@@ -7,28 +7,28 @@ do
 
 procesos=$(ps aux| grep LEADING | wc -l)
 if [ $procesos -eq 1 ]; then
-    python3 scripts_leading/LEADING_createCommunityUserByProfile_20240418.py
+    python3 calculo_coeficientes_reparto/LEADING_createCommunityUserByProfile_20240418.py
     echo "createCommunityUserByProfile"
 fi
 sleep 1
 
 procesos=$(ps aux| grep LEADING | wc -l)
 if [ $procesos -eq 1 ]; then
-    python3 scripts_leading/LEADING_getEstimacionProduccion_PVGIS_20240417.py
+    python3 calculo_coeficientes_reparto/LEADING_getEstimacionProduccion_PVGIS_20240417.py
     echo "getEstimacionProduccion"
 fi
 sleep 1
 
 procesos=$(ps aux| grep LEADING | wc -l)
 if [ $procesos -eq 1 ]; then
-    python3 scripts_leading/LEADING_script_matriz_energ_baterias_20240418.py
+    python3 calculo_coeficientes_reparto/LEADING_script_matriz_energ_baterias_20240418.py
     echo "script_matriz_energ_baterias"
 fi
 sleep 1
 
 procesos=$(ps aux| grep LEADING | wc -l)
 if [ $procesos -eq 1 ]; then
-    python3 scripts_leading/LEADING_COEF/AEI_LEADING_CaracterizacionComunidadesEnergeticasTask_v6.py
+    python3 calculo_coeficientes_reparto/LEADING_COEF/AEI_LEADING_CaracterizacionComunidadesEnergeticasTask_v6.py
     echo "CaracterizacionComunidadesEnergeticasTask"
 fi
 sleep 1
